55999528ad4e77aa337fca0eb49853ca9e23ba0f
Tamar Moshe <tamarmoshe14@gmail.com>
2021-02-17 16:26:23 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+        <VideoDetail :video="selectedVideo"></VideoDetail>
+        <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+const API_KEY = 'AIzaSyAwDvRGmjOcyJTZBQlsXJ6voLn4istbN3k';
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    data(){return{
+        videos:[],
+        selectedVideo: null
+    }},
+    components:{
+        SearchBar,
+        VideoList,
+        VideoDetail
+    },
+    methods: {
+        onVideoSelect(video){
+            this.selectedVideo = video;
+        },
+        onTermChange(searchTerm){
+        axios.get('https://www.googleapis.com/youtube/v3/search', {
+            params:{
+                key: API_KEY,
+                type: 'video',
+                part: 'snippet',
+                q: searchTerm
+            }
+        }).then(response=>{this.videos = response.data.items});
+    }}
+    
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <li class="list-group-item media" @click="onVideoSelect">
+        <img class="mr-3" :src="thumbnailsUrl"/>
+        <div class="media-body">{{ video.snippet.title }}</div>
+        </li>
+</template>
+
+
+<script>
+export default {
+    name: 'VideoListItem',
+    props: ['video'], 
+    computed: {
+        thumbnailsUrl(){
+            return this.video.snippet.thumbnails.default.url;
+        }
+    },
+    methods: {onVideoSelect(){
+        this.$emit('videoSelect', this.video)
+    }}
+    
+};
+</script>
+
+<style scoped>
+li{
+    display: flex;
+    cursor:pointer;
+
+}
+li:hover{
+    background-color:#eee;
+
+}
+</style>
